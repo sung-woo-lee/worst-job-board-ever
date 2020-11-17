31609142518d522b81e3fc53a5aafdf977e3a872
Sung Woo <yashu3lee@gmail.com>
2020-11-17 11:05:09 -0500

    more dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,40 @@
 module.exports = {
-	siteMetadata: {
-		title: `Nicea`,
-		description: `Like Upwork, Fiverr, all those. Just 1000% better and more exclusive.`,
-		author: `Sung Woo`,
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`,
-			},
-		},
-		{
-			resolve: 'gatsby-source-strapi',
-			options: {
-				apiURL: `${process.env.API_URL}`,
-				contentTypes: ['company', 'project', 'contractor'],
-				queryLimit: 100,
-			},
-		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `Nicea - Top Talent and top Projects`,
-				short_name: `Nicea`,
-				start_url: `/`,
-				background_color: `#f2f2f2`,
-				display: `minimal-ui`,
-			},
-		},
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
-	],
+  siteMetadata: {
+    title: `Nicea`,
+    description: `Like Upwork, Fiverr, all those. Just 1000% better and more exclusive.`,
+    author: `Sung Woo`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: `${process.env.GATSBY_API_URL}`,
+        contentTypes: ["company", "project", "contractor"],
+        queryLimit: 100,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nicea - Top Talent and top Projects`,
+        short_name: `Nicea`,
+        start_url: `/`,
+        background_color: `#f2f2f2`,
+        display: `minimal-ui`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
