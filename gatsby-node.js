fcdd1a518a28ec79f535b8f55ae25bdcb17d9cac
Sung Woo <yashu3lee@gmail.com>
2020-12-01 09:30:58 -0500

    fixed dotenv stuff that disappeared

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,28 @@
-const path = require("path");
+require('dotenv').config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
+const path = require('path');
 
 const makeRequest = (graphql, request) =>
-  new Promise((resolve, reject) => {
-    resolve(
-      graphql(request).then((result) => {
-        if (result.errors) {
-          reject(result.errors);
-          console.log(`check ${process.env.GATSBY_API_URL}`);
-        }
-        console.log(JSON.stringify(result));
-        return result;
-      })
-    );
-  });
+	new Promise((resolve, reject) => {
+		resolve(
+			graphql(request).then((result) => {
+				if (result.errors) {
+					reject(result.errors);
+					console.log(`check ${process.env.GATSBY_API_URL}`);
+				}
+				console.log(JSON.stringify(result));
+				return result;
+			})
+		);
+	});
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const allProjects = await makeRequest(
-    graphql,
-    `
+	const { createPage } = actions;
+	const allProjects = await makeRequest(
+		graphql,
+		`
     query {
       allStrapiProject {
         edges {
@@ -34,19 +38,19 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `
-  );
+	);
 
-  const createAllProjectPages = allProjects.data.allStrapiProject.edges.forEach(
-    ({ node }) => {
-      createPage({
-        path: `${node.id}`,
-        component: path.resolve(`./src/templates/projects.js`),
-        context: {
-          id: node.id,
-        },
-      });
-    }
-  );
+	const createAllProjectPages = allProjects.data.allStrapiProject.edges.forEach(
+		({ node }) => {
+			createPage({
+				path: `${node.id}`,
+				component: path.resolve(`./src/templates/projects.js`),
+				context: {
+					id: node.id,
+				},
+			});
+		}
+	);
 
-  return createAllProjectPages;
+	return createAllProjectPages;
 };
